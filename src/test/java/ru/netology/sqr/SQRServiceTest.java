809abd601e08fa16ccdb680beb3631211a4a8978
Helena <v0robjeva.alena@yandex.ru>
2023-01-01 19:18:57 +0300

    Cycles

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,29 +1,32 @@
 package ru.netology.sqr;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SQRServiceTest {
     @Test
-    public void calcTestWhenSomeNumbersFinded () {
+    public void calcTestWhenSomeNumbersFinded() {
         SQRService service = new SQRService();
         int actual = service.calc(200, 300);
         int expected = 3;
-        Assertion.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void calcTestWhenSomeNumbersNotFinded () {
+    public void calcTestWhenSomeNumbersNotFinded() {
         SQRService service = new SQRService();
         int actual = service.calc(10_000, 20_000);
         int expected = 0;
-        Assertion.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void calcTestWhenSomeNumbersNotFinded () {
+    public void calcTestWhenSomeNumbNotFinded() {
         SQRService service = new SQRService();
         int actual = service.calc(100, 105);
         int expected = 1;
-        Assertion.assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
